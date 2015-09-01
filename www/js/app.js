ebec553dab0b6397cbe3a47f35e565afc1332392
Axel Magnuson <axelmagn@gmail.com>
2015-08-31 20:47:23 -0600

    copy over old prototype code

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,52 @@
 // Ionic Starter App
 
+
+/*
+   var ListItem = function(name, description) {
+   this.type = "ListItem";
+   this.name = name;
+   this.description = description;
+   this.children = [];
+   }
+
+
+   ListItem.prototype.push = function(child) {
+   if(child.type !== "ListItem") {
+   throw new Error("Wrong Child Type");
+   }
+   this.children.push(child);
+   }
+   */
+
+var ListItem = function(name, description) {
+  var type = "ListItem";
+  var name = name;
+  var description = description;
+  var children = [];
+  return {
+    "push": function(child) {
+      if(child.type !== "ListItem") {
+        throw new Error("Wrong Child Type");
+      }
+      children.push(child);
+    }
+  }
+};
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('listr', ['ionic'])
+
+.controller('ListController', function() {
+  this.rootList = new ListItem("The Five Negative Thinkings", "TODO");
+  this.rootList.push(new ListItem("Anger", "TODO"));
+  this.rootList.push(new ListItem("Jealousy", "TODO"));
+  this.rootList.push(new ListItem("Greed", "TODO"));
+  this.rootList.push(new ListItem("Pride", "TODO"));
+  this.rootList.push(new ListItem("Ignorance", "TODO"));
+
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {

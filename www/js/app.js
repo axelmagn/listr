ebc222bd47f4034875c7db6cda22c0f4c900d4d1
Axel Magnuson <axelmagn@gmail.com>
2015-09-01 08:21:50 -0600

    clean up angular javascript

--- www/js/app.js
+++ www/js/app.js
@@ -1,62 +1,35 @@
-// Ionic Starter App
+(function() {
+  // angular.module is a global place for creating, registering and retrieving Angular modules
+  // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+  // the 2nd parameter is an array of 'requires'
+  var app = angular.module('listr', ['ionic']);
 
+  app.controller('ListController', function() {
 
-var ListItem = function(name, description) {
-  this.type = "ListItem";
-  this.name = name;
-  this.description = description;
-  this.children = [];
-}
-
-
-ListItem.prototype.push = function(child) {
-  if(child.type !== "ListItem") {
-    throw new Error("Wrong Child Type");
-  }
-  this.children.push(child);
-}
-
-/*
-   var ListItem = function(name, description) {
-   var type = "ListItem";
-   var name = name;
-   var description = description;
-   var children = [];
-   return {
-   "push": function(child) {
-   if(child.type !== "ListItem") {
-   throw new Error("Wrong Child Type");
-   }
-   children.push(child);
-   }
-   }
-   };
-   */
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('listr', ['ionic'])
-
-.controller('ListController', function() {
-  this.rootList = new ListItem("The Five Negative Thinkings", "TODO");
-  this.rootList.push(new ListItem("Anger", "TODO"));
-  this.rootList.push(new ListItem("Jealousy", "TODO"));
-  this.rootList.push(new ListItem("Greed", "TODO"));
-  this.rootList.push(new ListItem("Pride", "TODO"));
-  this.rootList.push(new ListItem("Ignorance", "TODO"));
-
-})
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+    this.active_list = {
+      "name": "Negative Thinking",
+      "children": [
+        { "name": "Anger", "hasChildren": false },
+        { "name": "Jealousy", "hasChildren": false },
+        { "name": "Greed", "hasChildren": false },
+        { "name": "Pride", "hasChildren": false },
+        { "name": "Ignorance", "hasChildren": false },
+      ]
+    
     }
   });
-})
+
+  app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+})();

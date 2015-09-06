a95c16de9c48d41cff55780594db30b045e8fe5f
Axel Magnuson <axelmagn@gmail.com>
2015-09-06 15:31:26 -0600

    Start integrating pouchdb

--- www/js/service.js
+++ www/js/service.js
@@ -1,11 +1,17 @@
 (function() {
-  var srvc = angular.module('listr-srvc', []);
+  var srvc = angular.module('listr-srvc', ['pouchdb']);
 
-  var CONFIG_URL = '/js/config.json';
 
   // service for retrieving and storing lists
-  srvc.factory('lists', [function() {
+  srvc.factory('lists', ['config', 'pouchDB', function(config, pouchDB) {
     var listService = {};
+
+    var cfg = config.get();
+    var dbname = cfg.resources.pouchdb.lists;
+    if(!_.isString(dbname)) {
+      throw new Error("Received a pouchdb dbname that isn't a string:\t", dbname);
+    }
+    var db = pouchDB(dbname);
 
     var dummyList = {
       "name": "Negative Thinking",
@@ -28,17 +34,28 @@
   }]);
 
 
+  /**
+   * Service for retrieving app config
+   */
   srvc.factory('config', ['$http', function($http) {
+    var CONFIG_URL = '/js/config.json';
     var cfg = {};
+    var data;
+
+    $http.get(CONFIG_URL).then(function(response) {
+      data = response.data;
+    });
 
     /**
-     * Get the JSON configuration object for this app.
-     * Returns a future of the response object
-     * @returns {future}
+     * Get the lazy-loaded JSON configuration object for this app.
+     * @returns {Object}
      */
     cfg.get = function() {
-      return $http.get(CONFIG_URL);
+      return data;
     };
+
+    return cfg;
   }]);
 
 })();
+

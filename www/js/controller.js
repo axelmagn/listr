e757b53fba011f0da3d497eed40a4f44ad533b6a
Axel Magnuson <axelmagn@gmail.com>
2015-09-06 21:47:39 -0600

    Integrate app with pouchdb storage

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,10 +1,17 @@
 (function() {
   var ctrl = angular.module('listr-ctrl', ['listr-srvc']);
 
-  var listController = ctrl.controller('ListController', ['$scope', '$stateParams', 'lists', 
-                                       function($scope, $stateParams, lists) {
-    $scope.url_list = $stateParams.listID ? $stateParams.listID : "root";
-    $scope.active_list = lists.get($scope.url_list);
+  var listController = ctrl.controller('ListController', ['$scope', '$stateParams', '$log', 'lists', 
+                                       function($scope, $stateParams, $log, lists) {
+    var listID = $stateParams.listID ? $stateParams.listID : null;
+    $scope.pageTitle = "LOADING";
+    lists.get(listID).then(function(list) {
+      // $log.debug("List:\t", list);
+      $scope.active_list = list;
+      $scope.pageTitle = $scope.active_list.name;
+    }, function(err) {
+      $log.error("Could not retrieve list ", listID, ":\t", err);
+    });
     // console.log('url_list:\t', $scope.url_list);
   }]);
 })();
--- www/js/service.js
+++ www/js/service.js
@@ -9,11 +9,33 @@
     }
   });
 
+  srvc.constant('designDoc', {
+    _id: '_design/listr',
+    views: {
+      children: {
+        map: function(doc) {
+          if(doc.type === "list_item") {
+            emit(doc.parent);
+          };
+        }.toString()
+      }
+    }
+  });
 
-  // service for retrieving and storing lists
-  srvc.factory('lists', ['config', 'pouchDB', function(config, pouchDB) {
-    var listService = {};
+  srvc.constant('dummyDocs', [
+    {_id: 'negative_thinking', type: "list_item", name: "Negative Thinking", parent: null },
+    {_id: 'anger', type: "list_item", name: "Anger", parent: 'negative_thinking' },
+    {_id: 'jealousy', type: "list_item", name: "Jealousy", parent: 'negative_thinking' },
+    {_id: 'greed', type: "list_item", name: "Greed", parent: 'negative_thinking' },
+    {_id: 'pride', type: "list_item", name: "Pride", parent: 'negative_thinking' },
+    {_id: 'ignorance', type: "list_item", name: "Ignorance", parent: 'negative_thinking' },
+  ]);
 
+  srvc.constant('rootItem', { name: "Lists" });
+
+
+  srvc.factory('listrPouchDB', ['config', 'designDoc', 'dummyDocs', 'pouchDB', '$log', 
+               function(config, designDoc, dummyDocs, pouchDB, $log) {
     // get or create lists database
     var dbname = config.resources.pouchdb.lists;
     if(!_.isString(dbname)) {
@@ -21,24 +43,67 @@
     }
     var db = pouchDB(dbname);
 
-    
+    // save the design document to the database
+    // TODO: handle DB that has already been initialized
+    db.put(designDoc).then(function() {
+      $log.info('Saved Design Doc to PouchDB');
+    }).catch(function(err) {
+      $log.error('Failed Saving Design Doc to PouchDB:\t', err);
+    });
+
+    // save dummy data
+    db.bulkDocs(dummyDocs).then(function() {
+      $log.info('Saved Dummy Docs to PouchDB');
+    }).catch(function(err) {
+      $log.error('Failed Saving Dummy Docs to PouchDB:\t', err);
+    });
 
 
-    var dummyList = {
-      "name": "Negative Thinking",
-      "slug": "negative_thinking",
-      "children": [
-        { "name": "Anger", "hasChildren": false },
-        { "name": "Jealousy", "hasChildren": false },
-        { "name": "Greed", "hasChildren": false },
-        { "name": "Pride", "hasChildren": false },
-        { "name": "Ignorance", "hasChildren": false },
-      ]
-    };
+    return db;
+  }]);
 
-    // method to retrieve a list
+
+  // service for retrieving and storing lists
+  srvc.factory('lists', ['$log', 'listrPouchDB', 'rootItem', function($log, listrPouchDB, rootItem) {
+    var listService = {};
+
+    var db = listrPouchDB;
+
+    /** 
+     * method to retrieve a list
+     * @return {future}
+     */
     listService.get = function(listID) {
-      return dummyList;
+
+      // if listID is null, get root lists
+      if(listID === null) {
+        var item = rootItem;
+        // get list children from ID
+        return db.query('listr/children', {
+          include_docs: true,
+          key: listID
+        }).then(function(children) {
+          item.children = _.pluck(children.rows, 'doc');
+          // $log.debug("Children:\t", children);
+          // $log.debug("Root Item:\t", item);
+          return item;
+        });
+      }
+
+      // get list from ID
+      return db.get(listID).
+        then(function(item) {
+          // get list children from ID
+          return db.query('listr/children', {
+            include_docs: true,
+            key: listID
+          }).then(function(children) {
+            item.children = _.pluck(children.rows, 'doc');
+            // $log.debug("Children:\t", children);
+            // $log.debug("Item:\t", item);
+            return item;
+          });
+        });
     };
 
     return listService;
